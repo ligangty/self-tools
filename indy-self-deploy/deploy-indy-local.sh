ee3af4a3215a13d153d529d7b6ae7544c721fc80
Gang Li <gli@redhat.com>
2017-09-19 21:13:50 +0800

    Initial commit: indy deploy local and jenkins plugins compare

---
+++ indy-self-deploy/deploy-indy-local.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+# used to deploy indy from a archive on local, and keep all configuration 
+# and storage not changed
+
+if [ "x$1" == "x" ]; then
+  echo "usage: $0 indy-launcher.tar.gz";
+  exit 0;
+fi
+
+if [ ! -f $1 ]; then
+  echo "$1 is not a regular file!";
+  exit 0;
+fi
+
+INDY_SRC=/tmp/$1
+OLD_INDY_LOC=$HOME/deploy/indy
+
+if [[ $1 == *.gz ]]; then
+  mkdir $INDY_SRC 
+  echo "extracting package to $INDY_SRC";
+  tar -zxvf $1 -C $INDY_SRC;
+elif [[ $1 == *.zip ]]; then
+  mkdir $INDY_SRC 
+  echo "extracting package to $INDY_SRC";
+  unzip $1 -d $INDY_SRC;
+else
+  echo "$1 is not a regular indy launcher package!";
+  exit 0;
+fi
+
+# stop old indy
+echo "stopping old indy instance"
+kill `ps -ef | grep -v grep | grep indy | grep java | awk '{print $2}'`
+
+# backup old indy
+#DATE=`date +%Y-%m-%d`
+#tar -zcvf ~/deploy/backup/indy-backup-$DATE.tar.gz $OLD_INDY_LOC/*
+
+
+# replace indy with new archives
+echo "replacing lib"
+rm -rf $OLD_INDY_LOC/lib
+cp -r $INDY_SRC/indy/lib $OLD_INDY_LOC/
+
+echo "replacing bin"
+rm -rf  $OLD_INDY_LOC/bin/indy.sh $OLD_INDY_LOC/bin/boot.properties $OLD_INDY_LOC/bin/init
+cp -r $INDY_SRC/indy/bin/* $OLD_INDY_LOC/bin/
+
+echo "replacing doc"
+rm -rf $OLD_INDY_LOC/usr/share/doc
+cp -r $INDY_SRC/indy/usr/share/doc $OLD_INDY_LOC/usr/share/
+
+echo "replacing uis"
+rm -rf $OLC_INDY_LOC/var/lib/indy/ui
+cp -r $INDY_SRC/indy/var/lib/indy/ui $OLD_INDY_LOC/var/lib/indy/
+
+# start indy
+echo "starting new indy instance"
+nohup $OLD_INDY_LOC/bin/indy.sh >> $OLD_INDY_LOC/var/log/indy/indy.log 2>&1 &
+
+# remove temp extracted files
+echo "remove temp extracted files"
+rm -rf $INDY_SRC
+
---
+++ jenkins-mig/compare-jenkins-plugins.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+# This script is used to check the differences of the plugins installation
+# between two jenkins instances
+
+import xml.etree.ElementTree as ET
+from distutils.version import LooseVersion, StrictVersion
+from argparse import ArgumentParser
+
+
+class PluginInfo(object):
+    def __init__(self, shortName, longName, version):
+        self.shortName = shortName
+        self.longName = longName
+        self.version = version
+
+    def __str__(self):
+        return 'shortName: "%s", longName: "%s", version: "%s"' % (self.shortName, self.longName, self.version)
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.shortName == other.shortName and self.version == other.version
+
+    def __ne__(self, other):
+        return (not self.__eq__(other))
+
+    def __hash__(self):
+        return hash(self.shortName) ^ hash(self.version)
+
+    __repr__ = __str__
+
+def run(target_xml, source_xml):
+    root_base = ET.parse(target_xml).getroot()
+    allPis_base = generate_plugins(root_base)
+    root_compare = ET.parse(source_xml).getroot()
+    allPis_compare = generate_plugins(root_compare)
+    installed = []
+    others = []
+    need_upgraded = []
+    not_installed = []
+    allPis_base_names = set(map(lambda x: x.shortName, allPis_base))
+    for pi in allPis_compare:
+        if pi in allPis_base:
+            installed.append(pi)
+        else:
+            for pi_base in allPis_base:
+                if pi_base.shortName == pi.shortName and version_compare(pi_base.version, pi.version):
+                    need_upgraded.append(pi)
+
+        if pi.shortName not in allPis_base_names:
+            not_installed.append(pi)
+
+    print_result(target_xml, source_xml, installed, need_upgraded, not_installed)
+
+def parse_args():
+  parser = ArgumentParser()
+  parser.add_argument('target_jenkins_plugins_xml_file', type=str,
+                   help='The target jenkins plugins xml result file which will be migrated to')
+  parser.add_argument('source_jenkins_plugins_xml_file', type=str,
+                   help='The source jenkins plugins xml result file which will be migrated from')
+
+  args = parser.parse_args()
+  return args.target_jenkins_plugins_xml_file, args.source_jenkins_plugins_xml_file
+
+
+
+def generate_plugins(root):
+    allPis = set()
+    for atype in root.findall('plugin'):
+        if atype.find('active').text == 'true' and atype.find('enabled').text == 'true':
+            allPis.add(PluginInfo(atype.find('shortName').text, atype.find('longName').text, atype.find('version').text))
+    return allPis
+
+def version_compare(v1, v2):
+    return LooseVersion(v1) < LooseVersion(v2)
+
+def print_result(target_xml, source_xml,installed, need_upgraded, not_installed):
+    print 'both installed:'
+    for pi in installed:
+        print '    %s' % pi
+
+    print 'not installed on %s but on %s:' % (target_xml, source_xml)
+    for pi in not_installed:
+        print '    %s' % pi
+
+    print 'need upgrade on %s:' % target_xml
+    for pi in need_upgraded:
+        print '    %s' % pi
+
+if __name__ == '__main__':
+    target_xml,source_xml = parse_args()
+    run(target_xml, source_xml)
